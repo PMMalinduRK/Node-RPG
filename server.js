4315619761a72593ea662e9823e62f47961a5f28
PMMalinduRK <120358431+PMMalinduRK@users.noreply.github.com>
2023-01-02 11:12:47 +0530

    Completed user authentication
    Reference: https://www.bezkoder.com/node-js-mongodb-auth-jwt/

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
+const cors = require("cors");
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
-//const port = process.env.PORT || 3000;
-const port = 3000;
 const path = require('path');
 let socketIo = require("socket.io");
-const { MongoClient } = require('mongodb');
+
+var corsOptions = {
+    origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(express.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
 
 // Connecting static file path to express
 app.use(express.static(path.join(__dirname, "resources")));
@@ -21,6 +31,27 @@ app.get('/main', (req, res) => {
 app.get('/match', (req, res) => {
     res.sendFile(path.join(__dirname, '/resources/match.html'));
 });
+require('./app/routes/auth.routes')(app);
+require('./app/routes/user.routes')(app);
+
+
+const db = require("./app/models");
+const Role = db.role;
+
+db.mongoose
+  .connect("mongodb+srv://MalinduRK:e0r2SWjof7Yh8e98@rpg-cluster.nb4qi3z.mongodb.net/Node_RPG?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connect to MongoDB.");
+    initial();
+  })
+  .catch(err => {
+    console.error("Connection error", err);
+    process.exit();
+  });
+
 
 // Setup the websocket.
 let io = socketIo(server);
@@ -39,46 +70,45 @@ io.on("connection", function(socket) {
 });
 
 
-
-
-async function main(username, password){
-    /**
-     * Connection URI. Update <username>, <password>, and <your-cluster-url> to reflect your cluster.
-     * See https://docs.mongodb.com/ecosystem/drivers/node/ for more details
-     */
-    const uri = "mongodb+srv://MalinduRK:e0r2SWjof7Yh8e98@rpg-cluster.nb4qi3z.mongodb.net/Node-RPG?retryWrites=true&w=majority";
-
-    const client = new MongoClient(uri);
-
-    try {
-        // Connect to the MongoDB cluster
-        await client.connect();
-
-        // Search for existing data
-        let exists = client.db('Node-RPG').collection('players').find(
-            { "username" : username }
-        );
-
-        console.log(exists);
-
-        // Add data
-        await client.db('Node-RPG').collection('players').insertOne({
-            username: username,
-            password: password
-        });
-
-    } catch (e) {
-        console.error(e);
-
-    } finally {
-        await client.close();
-    }
-}
-
-
-
-
-
-server.listen(port, () => {
-  console.log(`listening on port ${port}`);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
+
+
+function initial() {
+  Role.estimatedDocumentCount((err, count) => {
+    if (!err && count === 0) {
+      new Role({
+        name: "user"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'user' to roles collection");
+      });
+
+      new Role({
+        name: "moderator"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'moderator' to roles collection");
+      });
+
+      new Role({
+        name: "admin"
+      }).save(err => {
+        if (err) {
+          console.log("error", err);
+        }
+
+        console.log("added 'admin' to roles collection");
+      });
+    }
+  });
+}
